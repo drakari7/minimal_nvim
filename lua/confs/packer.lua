7c39ec32d4cb720c457dd4f0801e1db14d44ef79
drakari7 <shreyash.ravi@gmail.com>
2022-07-29 15:37:21 +0200

    lala

--- lua/confs/packer.lua
+++ lua/confs/packer.lua
@@ -15,49 +15,49 @@ return require('packer').startup(function(use)
   use {'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
-  use 'nvim-treesitter/playground'
+  -- use 'nvim-treesitter/playground'
 
   -- Functionality plugins
   use 'windwp/nvim-autopairs'               -- Autoclose brackets
   use 'famiu/bufdelete.nvim'                -- Delete buffers nicely
-  use 'tyru/open-browser.vim'               -- Open links with gx
+  -- use 'tyru/open-browser.vim'               -- Open links with gx
   use 'tpope/vim-unimpaired'                -- Symmetric mappings
   use 'tpope/vim-commentary'                -- Easy comments
   use 'tpope/vim-surround'                  -- change surroundings
   use 'tpope/vim-repeat'                    -- For repeating stuff
-  use 'mhinz/vim-startify'                  -- Startpage
-  use 'folke/which-key.nvim'                -- easy keymap guide
-  use 'lukas-reineke/indent-blankline.nvim' -- Indentation lines
+  -- use 'mhinz/vim-startify'                  -- Startpage
+  -- use 'folke/which-key.nvim'                -- easy keymap guide
+  -- use 'lukas-reineke/indent-blankline.nvim' -- Indentation lines
 
-  use {'kyazdani42/nvim-tree.lua',          -- File tree
-    requires = 'kyazdani42/nvim-web-devicons' -- Icons
-  }
+  -- use {'kyazdani42/nvim-tree.lua',          -- File tree
+  --   requires = 'kyazdani42/nvim-web-devicons' -- Icons
+  -- }
   -- Temporary and homeless plugins
-  use 'dstein64/vim-startuptime'            -- Measure startup performance
+  -- use 'dstein64/vim-startuptime'            -- Measure startup performance
 
   -- Git integration
-  use {
-    'lewis6991/gitsigns.nvim'
-  }
-  use 'tpope/vim-fugitive'                  -- Git commands in nvim
-  use 'tpope/vim-rhubarb'                   -- Gives Gbrowse command
+  -- use {
+  --   'lewis6991/gitsigns.nvim'
+  -- }
+  -- use 'tpope/vim-fugitive'                  -- Git commands in nvim
+  -- use 'tpope/vim-rhubarb'                   -- Gives Gbrowse command
 
   -- SpeedUp
-  use 'nathom/filetype.nvim'
-  use 'lewis6991/impatient.nvim'
+  -- use 'nathom/filetype.nvim'
+  -- use 'lewis6991/impatient.nvim'
 
   -- LSP related plugins
   use 'simrat39/symbols-outline.nvim'       -- Symbol table
   use 'ray-x/lsp_signature.nvim'            -- Function signatures
-  use 'jose-elias-alvarez/null-ls.nvim'     -- Formatting
+  -- use 'jose-elias-alvarez/null-ls.nvim'     -- Formatting
 
   -- Debugging
-  use 'mfussenegger/nvim-dap'               -- Debugging engine
-  use 'rcarriga/nvim-dap-ui'                -- UI and overlay
-  use 'theHamsta/nvim-dap-virtual-text'     -- Virtual text summary
+  -- use 'mfussenegger/nvim-dap'               -- Debugging engine
+  -- use 'rcarriga/nvim-dap-ui'                -- UI and overlay
+  -- use 'theHamsta/nvim-dap-virtual-text'     -- Virtual text summary
 
   -- Terminal wrapper
-  use 'akinsho/toggleterm.nvim'             -- better terminal
+  -- use 'akinsho/toggleterm.nvim'             -- better terminal
 
   -- Completion Plugins
   use 'hrsh7th/nvim-cmp'
@@ -72,28 +72,19 @@ return require('packer').startup(function(use)
   -- Snippets
   -- use 'SirVer/ultisnips'
   -- use 'honza/vim-snippets'
-  use 'L3MON4D3/LuaSnip'
-  use 'rafamadriz/friendly-snippets'        -- Some default snippets
+  -- use 'L3MON4D3/LuaSnip'
+  -- use 'rafamadriz/friendly-snippets'        -- Some default snippets
 
   -- Telescope
-  use 'nvim-telescope/telescope.nvim'       --File finder and grep
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use "nvim-telescope/telescope-file-browser.nvim"
+  -- use 'nvim-telescope/telescope.nvim'       --File finder and grep
+  -- use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  -- use "nvim-telescope/telescope-file-browser.nvim"
 
   -- Colors and aesthetics
-  use 'norcalli/nvim-colorizer.lua'         -- Color hexcodes
+  -- use 'norcalli/nvim-colorizer.lua'         -- Color hexcodes
   use 'ellisonleao/gruvbox.nvim'
 
   -- Statusline and bufferline
   use 'nvim-lualine/lualine.nvim'
   use 'akinsho/bufferline.nvim'
-
-
-  -- Language specific plugins
-  use 'lervag/vimtex'                       -- Latex
-  use {
-    'iamcco/markdown-preview.nvim',         -- Markdown preview
-    run = function() vim.fn['mkdp#util#install']() end,
-    ft = {'markdown'}
-  }
 end)
