dc75b3676c62ba9386a61edbf46543aad364d2ae
drakari7 <shreyash.ravi@gmail.com>
2022-07-29 15:27:20 +0200

    added unused configs

---
+++ lua/confs/dap.lua
@@ -0,0 +1,108 @@
+local dap = require('dap')
+
+require("nvim-dap-virtual-text").setup()
+
+dap.adapters.python = {
+  type = 'executable',
+  command = os.getenv('HOME') .. '/.virtualenvs/debugpy/bin/python',
+  args = { '-m', 'debugpy.adapter' },
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch',
+    name = "Launch file",
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}", -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local vdir = os.getenv('VIRTUAL_ENV')
+      if vdir then
+        return vdir .. '/bin/python'
+      else
+        return '/usr/bin/python3'
+      end
+    end
+  },
+}
+
+dap.adapters.lldb = {
+  type = 'executable',
+  command = '/usr/local/opt/llvm/bin/lldb-vscode', -- adjust as needed
+  name = "lldb"
+}
+dap.configurations.cpp = {
+  {
+    name = "Launch",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+
+    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --
+    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --
+    -- Otherwise you might get the following error:
+    --
+    --    Error on launch: Failed to attach to the target process
+    --
+    -- But you should be aware of the implications:
+    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    runInTerminal = false,
+  },
+}
+
+
+-- If you want to use this for rust and c, add something like this:
+dap.configurations.c = dap.configurations.cpp
+-- dap.configurations.rust = dap.configurations.cpp
+
+
+-- nvim dap ui configs
+require("dapui").setup({
+  -- icons = { expanded = "?", collapsed = "?" },
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<CR>", "<2-LeftMouse>" },
+    open = "o",
+    remove = "d",
+    edit = "e",
+    repl = "r",
+  },
+  layouts = {
+    {
+      elements = {
+        { id = "scopes", size = 0.25 },
+        { id = "breakpoints", size = 0.20 },
+        { id = "stacks", size = 0.25 },
+        { id = "watches", size = 0.30 },
+      },
+      size = 50,
+      position = 'right',
+    },
+    {
+      elements = { 'repl' },
+      size = 10,
+      position = 'bottom',
+    },
+  },
+  floating = {
+    max_height = nil, -- These can be integers or a float between 0 and 1.
+    max_width = nil, -- Floats will be treated as percentage of your screen.
+    mappings = {
+      close = { "q", "<Esc>" },
+    },
+  },
+  windows = { indent = 1 },
+})
--- lua/confs/packer.lua
+++ lua/confs/packer.lua
@@ -15,36 +15,49 @@ return require('packer').startup(function(use)
   use {'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
+  use 'nvim-treesitter/playground'
 
   -- Functionality plugins
   use 'windwp/nvim-autopairs'               -- Autoclose brackets
   use 'famiu/bufdelete.nvim'                -- Delete buffers nicely
-  -- use 'tyru/open-browser.vim'               -- Open links with gx
+  use 'tyru/open-browser.vim'               -- Open links with gx
   use 'tpope/vim-unimpaired'                -- Symmetric mappings
   use 'tpope/vim-commentary'                -- Easy comments
   use 'tpope/vim-surround'                  -- change surroundings
   use 'tpope/vim-repeat'                    -- For repeating stuff
-  -- use 'lukas-reineke/indent-blankline.nvim' -- Indentation lines
+  use 'mhinz/vim-startify'                  -- Startpage
+  use 'folke/which-key.nvim'                -- easy keymap guide
+  use 'lukas-reineke/indent-blankline.nvim' -- Indentation lines
 
-  -- LSP related plugins
-  use 'simrat39/symbols-outline.nvim'       -- Symbol table
-  -- use 'folke/which-key.nvim'                -- easy keymap guide
-  -- use 'lukas-reineke/indent-blankline.nvim' -- Indentation lines
-
-  -- use {'kyazdani42/nvim-tree.lua',          -- File tree
-  --   requires = 'kyazdani42/nvim-web-devicons' -- Icons
-  -- }
+  use {'kyazdani42/nvim-tree.lua',          -- File tree
+    requires = 'kyazdani42/nvim-web-devicons' -- Icons
+  }
+  -- Temporary and homeless plugins
+  use 'dstein64/vim-startuptime'            -- Measure startup performance
 
   -- Git integration
-  -- use {
-  --   'lewis6991/gitsigns.nvim'
-  -- }
-  -- use 'tpope/vim-fugitive'                  -- Git commands in nvim
-  -- use 'tpope/vim-rhubarb'                   -- Gives Gbrowse command
+  use {
+    'lewis6991/gitsigns.nvim'
+  }
+  use 'tpope/vim-fugitive'                  -- Git commands in nvim
+  use 'tpope/vim-rhubarb'                   -- Gives Gbrowse command
+
+  -- SpeedUp
+  use 'nathom/filetype.nvim'
+  use 'lewis6991/impatient.nvim'
 
   -- LSP related plugins
   use 'simrat39/symbols-outline.nvim'       -- Symbol table
-  -- use 'ray-x/lsp_signature.nvim'            -- Function signatures
+  use 'ray-x/lsp_signature.nvim'            -- Function signatures
+  use 'jose-elias-alvarez/null-ls.nvim'     -- Formatting
+
+  -- Debugging
+  use 'mfussenegger/nvim-dap'               -- Debugging engine
+  use 'rcarriga/nvim-dap-ui'                -- UI and overlay
+  use 'theHamsta/nvim-dap-virtual-text'     -- Virtual text summary
+
+  -- Terminal wrapper
+  use 'akinsho/toggleterm.nvim'             -- better terminal
 
   -- Completion Plugins
   use 'hrsh7th/nvim-cmp'
@@ -53,16 +66,34 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
+  use 'saadparwaiz1/cmp_luasnip'
+  -- use 'quangnguyen30192/cmp-nvim-ultisnips'
+
+  -- Snippets
+  -- use 'SirVer/ultisnips'
+  -- use 'honza/vim-snippets'
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'        -- Some default snippets
 
   -- Telescope
-  -- use 'nvim-telescope/telescope.nvim'       --File finder and grep
-  -- use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  -- use "nvim-telescope/telescope-file-browser.nvim"
+  use 'nvim-telescope/telescope.nvim'       --File finder and grep
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use "nvim-telescope/telescope-file-browser.nvim"
 
   -- Colors and aesthetics
+  use 'norcalli/nvim-colorizer.lua'         -- Color hexcodes
   use 'ellisonleao/gruvbox.nvim'
 
   -- Statusline and bufferline
   use 'nvim-lualine/lualine.nvim'
   use 'akinsho/bufferline.nvim'
+
+
+  -- Language specific plugins
+  use 'lervag/vimtex'                       -- Latex
+  use {
+    'iamcco/markdown-preview.nvim',         -- Markdown preview
+    run = function() vim.fn['mkdp#util#install']() end,
+    ft = {'markdown'}
+  }
 end)
