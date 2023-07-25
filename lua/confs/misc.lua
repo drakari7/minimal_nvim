b85eb48f35fcce7eb0015d4b2f2feaf20becdeb2
shreyash <ravi@davincitrading.com>
2023-07-25 16:04:16 +0200

    remove leap

--- lua/confs/misc.lua
+++ lua/confs/misc.lua
@@ -1,7 +1,6 @@
 -- Miscellaneous plugin setups and keybindings
 
 -- Call simple plugin setups
-require('leap').add_default_mappings()
 require('Comment').setup()
 require('neodev').setup()
 require('indent_blankline').setup {
