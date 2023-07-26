7af5045218af1480182d5e40a8c60622417c734b
shreyash <ravi@davincitrading.com>
2023-07-26 19:12:22 +0200

    add some mini modules

--- lua/confs/mini.lua
+++ lua/confs/mini.lua
@@ -4,12 +4,16 @@ require('mini.bracketed').setup({
   diagnostic = { suffix = '' }
 })
 
-require('mini.bufremove').setup()
-map('n', '<leader>bd', require('mini.bufremove').delete, "Delete buffer")
 
-require('mini.ai').setup()
 require('mini.surround').setup({
   mappings = {
     replace = 'sc', -- Replace surrounding
   },
 })
+
+require('mini.bufremove').setup()
+map('n', '<leader>bd', require('mini.bufremove').delete, "Delete buffer")
+
+require('mini.ai').setup()
+require('mini.splitjoin').setup()
+require('mini.comment').setup()
--- lua/confs/misc.lua
+++ lua/confs/misc.lua
@@ -1,7 +1,6 @@
 -- Miscellaneous plugin setups and keybindings
 
 -- Call simple plugin setups
-require('Comment').setup()
 require('neodev').setup()
 require('indent_blankline').setup {
   -- show_current_context = true
