2e0a5138cd47261bc14d1b9f8e0c023abc8bb717
shreyash <ravi@davincitrading.com>
2023-07-28 17:11:10 +0200

    update flash keybindings

--- lua/confs/flash.lua
+++ lua/confs/flash.lua
@@ -1,2 +1,10 @@
 local flash = require('flash')
-flash.setup({})
+flash.setup()
+
+local map = require('confs.utils').map
+
+-- map('n', 's', flash.jump, 'Flash')
+-- map('n', 'S', flash.treesitter, 'Flash Treesitter')
+-- map('o', 'r', flash.remote, 'Remote Flash')
+-- map('o', 'R', flash.treesitter_search, 'Treesitter Search')
+-- map('c', '<c-s>', flash.toggle, 'Toggle Flash Search')
