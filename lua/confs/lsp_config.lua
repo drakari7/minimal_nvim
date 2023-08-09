2c24d19cd7768c999d9c6ebbcab0a639a3c1bf2b
shreyash <ravi@davincitrading.com>
2023-08-09 16:06:58 +0200

    allow formatting in visual mode

--- lua/confs/lsp_config.lua
+++ lua/confs/lsp_config.lua
@@ -1,4 +1,4 @@
-require('neodev').setup()           -- Must be included before lspconfig.setup
+require('neodev').setup() -- Must be included before lspconfig.setup
 require('mason').setup()
 require('mason-lspconfig').setup {
   automatic_installation = true,
@@ -6,20 +6,20 @@ require('mason-lspconfig').setup {
 
 -- ['─', '│', '─', '│', '┌', '┐', '┘', '└']
 local border = {
-    {"┌", "FloatBorder"},
-    {"─", "FloatBorder"},
-    {"┐", "FloatBorder"},
-    {"│", "FloatBorder"},
-    {"┘", "FloatBorder"},
-    {"─", "FloatBorder"},
-    {"└", "FloatBorder"},
-    {"│", "FloatBorder"},
+  { "┌", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "┐", "FloatBorder" },
+  { "│", "FloatBorder" },
+  { "┘", "FloatBorder" },
+  { "─", "FloatBorder" },
+  { "└", "FloatBorder" },
+  { "│", "FloatBorder" },
 }
 
 
 -- TODO: check if its possible to simplify this
 local handlers = {
-  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'}),
+  ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' }),
 }
 
 -- Change diagnostic settings
@@ -42,11 +42,11 @@ local lsp_signature_cfg = {
   },
 }
 
-local float_table = {float={border="rounded"}}
+local float_table = { float = { border = "rounded" } }
 local map = require('confs.utils').map
 
 -- TODO: Look into replacing this with lsp saga
-vim.diagnostic.config({signs = false, underline = true})
+vim.diagnostic.config({ signs = false, underline = true })
 map('n', '[d', function() vim.diagnostic.goto_prev(float_table) end, 'Prev diagnostic')
 map('n', ']d', function() vim.diagnostic.goto_next(float_table) end, 'Next diagnostic')
 map('n', '<leader>dl', vim.diagnostic.setloclist, 'Populate diagnostics in loclist')
@@ -58,9 +58,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     map('n', 'gd', vim.lsp.buf.definition, 'Goto definition', opts)
     map('n', 'gr', '<cmd>Telescope lsp_references initial_mode=normal<CR>', 'LSP references', opts)
     map('n', 'K', vim.lsp.buf.hover, 'Hover', opts)
-    map('n', 'gf', vim.lsp.buf.format, 'LSP Format', opts)
+    map({ 'n', 'v' }, 'gf', vim.lsp.buf.format, 'LSP Format', opts)
     map('n', '<leader>sr', vim.lsp.buf.rename, 'Rename symbol', opts)
-    map({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, 'Code action', opts)
+    map({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, 'Code action', opts)
 
     map('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, 'Add workspace folder', opts)
     map('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, 'Remove workspace folder', opts)
@@ -71,8 +71,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 local function on_attach(client, bufnr)
-  client.server_capabilities.semanticTokensProvider = nil       -- Disable semantic highlighting for now
-  require('lsp_signature').on_attach(lsp_signature_cfg)     -- Attach lsp signature
+  client.server_capabilities.semanticTokensProvider = nil -- Disable semantic highlighting for now
+  require('lsp_signature').on_attach(lsp_signature_cfg)   -- Attach lsp signature
 end
 
 local servers = {
@@ -92,7 +92,7 @@ local servers = {
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup ({
+  require('lspconfig')[lsp].setup({
     on_attach = on_attach,
     single_file_support = true,
     capabilities = capabilities,
