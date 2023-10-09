3979fb31e17cca3d6b4c604b74ec72bf2b074e7f
shreyash <ravi@davincitrading.com>
2023-10-09 13:48:55 +0200

    update indent blankline

--- lua/confs/misc.lua
+++ lua/confs/misc.lua
@@ -3,8 +3,6 @@
 -- Call simple plugin setups
 require('neodev').setup()
 require('Comment').setup()
-require('indent_blankline').setup ({
-  -- show_current_context = true
+require('ibl').setup({
+  exclude = { filetypes = { 'norg', 'help' } }
 })
-
-vim.g.indent_blankline_filetype_exclude = {'norg', 'help'}
