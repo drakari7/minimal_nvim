588f9b31773c1c0e726208a151d9d846b7ccd2a0
drakari7 <shreyash.ravi@gmail.com>
2023-10-21 03:19:13 +0200

    fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    dependencies = {"nvim-treesitter/nvim-treesitter-textobjects"},
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
     opts = {
-      ensure_installed = "all",
+      ensure_installed = {'lua', 'bash', 'c', 'cpp', 'python'},
+      sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
     }
