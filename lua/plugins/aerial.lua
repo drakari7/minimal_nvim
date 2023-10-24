fc6281057d16754a844929c045ae48d71d5cee7c
Shreyash Ravi <shreyash.ravi@gmail.com>
2023-10-24 21:53:08 +0200

    Make aerial lazy

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,53 +1,34 @@
 return {
   {
-    'stevearc/aerial.nvim',                -- Symbol table
+    'stevearc/aerial.nvim',
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons"
     },
-    config = function(_, opts)
-      local aerial = require('aerial')
-      aerial.setup(opts)
-      local map = require('confs.utils').map
-      map('n', '<leader>ao', function() aerial.toggle{focus=false} end, 'Aerial toggle')
-      map('n', '<leader>an', aerial.nav_toggle, 'Aerial nav toggle')
-      map('n', '<leader>af', aerial.focus, 'Aerial focus')
-      map('n', '<leader>ac', aerial.close_all, 'Aerial Close all')
-      map('n', ']a', aerial.next, 'Aerial next')
-      map('n', '[a', aerial.prev, 'Aerial prev')
-    end,
+    keys = {
+      { "<leader>ao", function() require('aerial').toggle({ focus = false }) end, desc = 'Aerial toggle' },
+      { "<leader>an", function() require('aerial').nav_toggle() end,           desc = 'Aerial nav toggle' },
+      { "<leader>af", function() require('aerial').focus() end,                desc = 'Aerial focus' },
+      { "<leader>ac", function() require('aerial').close_all() end,            desc = 'Aerial Close all' },
+      { "]a",         function() require('aerial').next() end,                 desc = 'Aerial next' },
+      { "[a",         function() require('aerial').prev() end,                 desc = 'Aerial prev' },
+    },
     opts = {
       layout = {
         max_width = { 30, 0.16 },
         min_width = { 15, 0.1 },
-        -- width = nil,
-        default_direction = "prefer_right",
-        placement = "window",
       },
 
-      filter_kind = {
-        "Class",
-        "Constructor",
-        "Enum",
-        "Function",
-        "Interface",
-        "Module",
-        "Method",
-        "Struct",
-      },
-
-      backends = { "treesitter", "markdown"},
-      close_automatic_events = {"unsupported"},
-      attach_mode = "window",
+      close_automatic_events = { "unsupported" },
       show_guides = true,
 
       manage_folds = true,
       link_tree_to_folds = true,
       -- link_folds_to_tree = true,
 
-      highlight_on_jump = false,     -- Set a value in milliseconds or false to disable
+      highlight_on_jump = false, -- Set a value in milliseconds or false to disable
       nav = {
-        min_height = {30, 0.6},
+        min_height = { 30, 0.6 },
         preview = true,
         win_opts = {
           cursorline = false,
