e0ff4a99c11c16c1a75fd2126d937bc29d922f6f
shreyash <ravi@davincitrading.com>
2023-10-24 13:15:41 +0200

    update treesitter list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs',
     opts = {
-      ensure_installed = {'lua', 'bash', 'c', 'cpp', 'python', 'norg'},
+      ensure_installed = {'lua', 'bash', 'c', 'cpp', 'python', 'norg', 'norg_meta'},
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
