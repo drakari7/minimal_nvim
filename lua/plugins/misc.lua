ae4659a6cd395dec972739ad5b7303e991044271
shreyash <ravi@davincitrading.com>
2023-12-07 23:55:47 +0100

    add oil keybinding and comment out nvimtree

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -49,5 +49,9 @@ return {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {},
+    lazy = false,
+    keys = {
+      { "<leader>oi", ":Oil<CR>", desc = "Open Oil filetree" }
+    }
   }
 }
