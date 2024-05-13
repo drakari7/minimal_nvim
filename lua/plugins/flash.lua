0f1aab5c4eb7a2d08a6290d1a89b88eaf6f73b82
shreyash <ravi@davincitrading.com>
2024-05-13 09:27:59 +0000

    disable flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,18 +1,23 @@
+-- TODO: Re enable this plugin when you can selectively use the features you want
+return {}
 
-return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {
-    backdrop = false,
-  },
-  -- keys = {
-  --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-  --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-  --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-  --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  -- },
-}
+-- return {
+--   "folke/flash.nvim",
+--   event = "VeryLazy",
+--   opts = {
+--     backdrop = false,
+--   },
+--   char = {
+--     enabled = false
+--   },
+--   -- keys = {
+--   --   { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+--   --   { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+--   --   { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+--   --   { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+--   --   { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+--   -- },
+-- }
 
 -- TODO: flash config file if i re enable flash
 -- local flash = require('flash')
