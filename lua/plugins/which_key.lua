11408d1ec62210a796ba21435c016d1e53e8beaf
shreyash <ravi@davincitrading.com>
2024-07-22 10:59:46 +0000

    update which key config

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -3,9 +3,8 @@ return {
     'folke/which-key.nvim',
     event = 'VeryLazy',
     opts = {
-      window = { border = "single" },
-      motions = { count = false },
-      triggers_nowait = {},
+      preset = "modern",
+      delay = 500
     },
   },
 }
