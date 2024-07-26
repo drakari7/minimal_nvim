9adaee233dc0b3ec350dae75588def62dec074c3
shreyash <ravi@davincitrading.com>
2024-07-26 11:35:32 +0000

    add telescope zoxide

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,18 @@
 return {
   -- Telescope
-  'nvim-telescope/telescope.nvim',       --File finder and grep
+  'nvim-telescope/telescope.nvim', --File finder and grep
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'jvgrootveld/telescope-zoxide',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
-  config = function ()
+  config = function()
     local telescope = require('telescope')
     local actions = require('telescope.actions')
     local builtin = require('telescope.builtin')
     local utils = require('confs.utils')
 
-    telescope.setup{
+    telescope.setup({
       defaults = {
         prompt_prefix = "   ",
         results_title = false,
@@ -21,7 +22,7 @@ return {
           preview_width = 0.47,
         },
 
-        file_ignore_patterns = {"__pycache__/", },
+        file_ignore_patterns = { "__pycache__/", },
         mappings = {
           i = {
             ["<c-j>"] = actions.move_selection_next,
@@ -43,27 +44,29 @@ return {
           "--smart-case",
         }
       },
-    }
+    })
+
     -- Extensions
     telescope.load_extension('fzf')
+    telescope.load_extension('zoxide')
 
     -- Custom pickers
     local function nvim_config_files()
-      builtin.find_files{
+      builtin.find_files {
         cwd = "~/.config/nvim",
         prompt_title = "Nvim Config Files",
       }
     end
 
     local function find_scripts()
-      builtin.find_files{
+      builtin.find_files {
         cwd = "~/scripts",
         prompt_title = "My Scripts",
       }
     end
 
     local function find_notes()
-      builtin.find_files{
+      builtin.find_files {
         cwd = "~/notes",
         prompt_title = "My Notes",
       }
@@ -80,9 +83,9 @@ return {
     end
 
     local function live_grep_from_git_root()
-      local opts = {path_display={'tail'}}
+      local opts = { path_display = { 'tail' } }
       if utils.is_git_repo() then
-        opts = vim.tbl_extend("keep", opts, {cwd=utils.get_git_root()})
+        opts = vim.tbl_extend("keep", opts, { cwd = utils.get_git_root() })
       end
       builtin.live_grep(opts)
     end
@@ -101,8 +104,9 @@ return {
     map('n', '<leader>fs', find_scripts, 'Scripts directory')
     map('n', '<leader>fn', find_notes, 'Notes directory')
     map('n', '<leader>fc', builtin.current_buffer_fuzzy_find, 'Current buffer search')
-    map('n', '<leader>fw', function() builtin.grep_string{initial_mode="normal"} end, 'Grep word under cursor')
-    map('n', '<leader>fr', function() builtin.resume{initial_mode="normal"} end, 'Resume last picker')
+    map('n', '<leader>fw', function() builtin.grep_string { initial_mode = "normal" } end, 'Grep word under cursor')
+    map('n', '<leader>fr', function() builtin.resume { initial_mode = "normal" } end, 'Resume last picker')
+    map('n', '<leader>ji', telescope.extensions.zoxide.list, "Zoxide cd")
 
     -- Git
     map('n', '<leader>gc', builtin.git_commits, 'Git commits')
