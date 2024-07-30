7a003d294f6fd40ac9227d0bc8e231ee9b62d94e
shreyash <ravi@davincitrading.com>
2024-07-30 11:23:54 +0000

    update source lib64 for crypto options

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,7 +4,7 @@ buf_map('n', '<leader>pr', '^yt<space>oprint(<C-r>")<ESC>', 'Print variable')
 buf_map('n', '<leader>di', 'viwoF,d', 'Delete import')
 buf_map('n', '<leader>oi', '<cmd>PyrightOrganizeImports<CR>', 'Organize imports')
 
-buf_map('n', '<leader>ib', function ()
+buf_map('n', '<leader>ib', function()
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   local boilerplate = {
     "async def main():",
@@ -13,13 +13,23 @@ buf_map('n', '<leader>ib', function ()
     'if __name__ == "__main__":',
     '    asyncio.run(main())'
   }
-  vim.api.nvim_buf_set_text(0, row-1, 0, row-1, 0, boilerplate)
+  vim.api.nvim_buf_set_text(0, row - 1, 0, row - 1, 0, boilerplate)
 end, 'Insert asyncio boilerplate')
 
+local function append_path(var, path_to_append)
+  local res = var and var or ""
+  return res .. ":" .. path_to_append
+end
 
-buf_map('n', '<leader>sc', function ()
-  local dist_test = ":/home/shreyash/colos/colo/dist-test/lib64"
-  vim.env.PYTHONPATH = vim.env.PYTHONPATH .. dist_test
-  vim.env.LD_LIBRARY_PATH = vim.env.LD_LIBRARY_PATH .. dist_test
-  print("Sourced " .. dist_test)
+buf_map('n', '<leader>sc', function()
+  local colo = "/home/shreyash/crypto_options/colo"
+  local lib64 = colo .. "/dist-gcc/lib64"
+  local typings = colo .. "/typings"
+  vim.env.PYTHONPATH = append_path(vim.env.PYTHONPATH, lib64)
+  vim.env.PYTHONPATH = append_path(vim.env.PYTHONPATH, typings)
+  vim.env.LD_LIBRARY_PATH = append_path(vim.env.LD_LIBRARY_PATH, lib64)
+  -- vim.lsp.stop_client(vim.lsp.get_clients())
+  -- vim.lsp.start_client(vim.lsp.get_clients())
+  -- print(vim.inspect(vim.lsp.get_clients()))
+  print("Sourced " .. lib64)
 end, 'Source lib64')
