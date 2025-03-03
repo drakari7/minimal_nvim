8a4ba5644e601df73535708af151be89d0b1445d
shreyash <ravi@davincitrading.com>
2025-03-03 12:09:27 -0500

    update ruff_lsp -> ruff

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -19,7 +19,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "pyright",
-          "ruff_lsp",
+          "ruff",
           "clangd",
           "bashls",
           "lua_ls",
