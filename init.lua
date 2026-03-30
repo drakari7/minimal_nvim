94e0e2a54283932d9f95812272327807dfc4ce69
Shreyash Ravi <shreyash.ravi@gmail.com>
2026-03-30 02:31:56 +0200

    comment out xsel kill autocommand

--- init.lua
+++ init.lua
@@ -7,9 +7,9 @@ require('confs.keybindings')
 require('confs.lazy')
 
 -- Kill xsel on leaving an instance of neovim
-vim.api.nvim_create_autocmd("VimLeave", {
-  group = vim.api.nvim_create_augroup("KillXSel", { clear = true }),
-  callback = function()
-    os.execute("killall xsel")
-  end
-})
+-- vim.api.nvim_create_autocmd("VimLeave", {
+--   group = vim.api.nvim_create_augroup("KillXSel", { clear = true }),
+--   callback = function()
+--     os.execute("killall xsel")
+--   end
+-- })
