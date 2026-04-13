3eef80478ba039395d0e3fc9ea54dabb380a3e19
Shreyash Ravi <sravi@block-tech.io>
2026-04-13 15:47:28 +0200

    fix colorscheme

--- init.lua
+++ init.lua
@@ -7,4 +7,6 @@ require('confs.keybindings')
 require('confs.lazy')
 
 vim.o.background = 'light'
+-- idk why but the tabs dont work unless i set colorscheme twice
+vim.cmd([[colorscheme gruvbox]])
 vim.cmd([[colorscheme gruvbox]])
