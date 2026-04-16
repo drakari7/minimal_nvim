233414037f657fdff9c8046ed718cf01d47bca95
Shreyash Ravi <sravi@block-tech.io>
2026-04-16 14:03:45 +0200

    add format on save for rust

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,7 +1,12 @@
 local buf_map = require('confs.utils').buf_map
 buf_map('n', '<leader>rr', ':w<CR>:!cargo run<CR>', 'Execute file')
 
-buf_map("n", "gra", function () vim.cmd.RustLsp("codeAction") end, "Code Action")
-buf_map("n", "K", function () vim.cmd.RustLsp({"hover", "actions"}) end, "Hover")
+buf_map("n", "gra", function() vim.cmd.RustLsp("codeAction") end, "Code Action")
+buf_map("n", "K", function() vim.cmd.RustLsp({ "hover", "actions" }) end, "Hover")
 
-
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = 0,
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
