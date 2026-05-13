fd6ce293614e54745d9b6329d0e4ec953127c1ee
Shreyash Ravi <sravi@block-tech.io>
2026-05-13 13:14:52 +0200

    sync plugins + vim.loop -> vim.uv

--- lua/confs/lazy.lua
+++ lua/confs/lazy.lua
@@ -1,6 +1,6 @@
 -- Install lazy if not present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
