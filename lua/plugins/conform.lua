2814067b052cca5eabf32c593073d25bc66b9112
Shreyash Ravi <sravi@block-tech.io>
2026-05-18 19:32:07 +0200

    fix black formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,29 +2,37 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd   = { 'ConformInfo' },
-  opts  = {
-    formatters_by_ft = {
-      python = { 'autoflake', 'isort', 'black' },
-      lua    = { 'stylua' },
-      sh     = { 'shfmt' },
-      bash   = { 'shfmt' },
-      rust   = { 'rustfmt' },
-    },
-    format_on_save = function(bufnr)
-      local enable_fts = {
-        rust = true,
-      }
-      if not enable_fts[vim.bo[bufnr].filetype] then
-        return
-      else
-        return {
-          timeout_ms = 1000,
-          -- lsp_format = 'fallback'
+  opts  = function()
+    local pyproject_root = require('conform.util').root_file({ 'pyproject.toml' })
+    return {
+      formatters_by_ft = {
+        python = { 'autoflake', 'isort', 'black' },
+        lua    = { 'stylua' },
+        sh     = { 'shfmt' },
+        bash   = { 'shfmt' },
+        rust   = { 'rustfmt' },
+      },
+      formatters = {
+        autoflake = { cwd = pyproject_root },
+        isort     = { cwd = pyproject_root },
+        black     = { cwd = pyproject_root },
+      },
+      format_on_save = function(bufnr)
+        local enable_fts = {
+          rust = true,
         }
-      end
-    end,
-  },
-  keys  = {
+        if not enable_fts[vim.bo[bufnr].filetype] then
+          return
+        else
+          return {
+            timeout_ms = 1000,
+            -- lsp_format = 'fallback'
+          }
+        end
+      end,
+    }
+  end,
+  keys = {
     {
       'gf',
       mode = { 'n', 'x' },
